2d3b0be8e26a1b6a8f6e466c2c51c7b794e41d11
Jake Wheeler <jake.wheeler@skylight.digital>
2024-10-17 11:15:08 -0400

    3.3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,27 +7,33 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+
+	app := &application{
+		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
+	}
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id...}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id...}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	logger.Info("starting server", slog.String("addr", *addr))
+	app.logger.Info("starting server", slog.String("addr", *addr))
 
 	err := http.ListenAndServe(*addr, mux)
 
-	logger.Error(err.Error())
+	app.logger.Error(err.Error())
 	os.Exit(1)
 }
