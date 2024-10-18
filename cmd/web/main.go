65d66116a7e288e0395aaa190ffde36e307759a2
Jake Wheeler <jake.wheeler@skylight.digital>
2024-10-18 09:53:08 -0400

    4.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type application struct {
@@ -12,18 +15,40 @@ type application struct {
 }
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	connStr := flag.String("dsn", "devuser:devpassword@tcp(127.0.0.1:3306)/snippetbox?parseTime=true", "MySQL data connection string")
+	flag.Parse()
 
 	app := &application{
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+	db, err := openDb(*connStr)
+	if err != nil {
+		app.logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app.logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 
 	app.logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDb(cs string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cs)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
